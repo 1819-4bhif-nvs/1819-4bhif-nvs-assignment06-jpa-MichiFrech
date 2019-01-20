fbc8d3becf9f52589b89e50b64de7a1ec5a65109
MichiFrech <michifrech2000@gmail.com>
2019-01-20 20:03:01 +0100

    Updated tests

--- Farm-st/src/test/java/at/htl/farm/FarmST.java
+++ Farm-st/src/test/java/at/htl/farm/FarmST.java
@@ -1,6 +1,5 @@
 package at.htl.farm;
 
-import org.hamcrest.Matchers;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -17,7 +16,8 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -25,62 +25,134 @@ import static org.junit.Assert.assertThat;
 public class FarmST {
     private static Client client;
     private static WebTarget target;
+    private static int tmpAnimalId = 0;
+    private static int tmpFieldId = 0;
 
 
     @BeforeClass
-    public static void init(){
+    public static void init() {
         client = ClientBuilder.newClient();
-        target = client.target("http://localhost:8085/farm/rs");
+        target = client.target("http://localhost:8080/farm/rs");
     }
 
     @Test
-    public void test01_GetAllFarms(){
-        Response response = this.target.path("/farm/findall").request(MediaType.APPLICATION_JSON).get();
+    public void test01_GetAllAnimals() {
+        Response response = this.target.path("/animal/findall").request(MediaType.APPLICATION_JSON).get();
         assertThat(response.getStatus(), is(200));
         JsonArray jsonArray = response.readEntity(JsonArray.class);
-        assertThat(jsonArray.getValuesAs(JsonObject.class).size(), greaterThanOrEqualTo(2));
-    }
-
-
-    @Test
-    public void test02_GetAnimalByName(){
-        JsonObject response = target.path("/animal/findByName/Manuel").request(MediaType.APPLICATION_JSON).get(JsonObject.class);
-        assertThat(response.getInt("age"), is(3));
-        assertThat(response.getString("fieldOfUse"), is("schlachten"));
+        assertThat(jsonArray.getValuesAs(JsonObject.class).size(), greaterThan(1));
     }
 
     @Test
-    public void test03_GetFieldBySeed(){
-        JsonObject response = target.path("/field/findBySeed/Weizen").request(MediaType.APPLICATION_JSON).get(JsonObject.class);
-        assertThat(response.getInt("hectare"), is(4));
+    public void test02_GetAnimal() {
+        Response response = this.target.path("/animal/find/1").request(MediaType.APPLICATION_JSON).get();
+        assertThat(response.getStatus(), is(200));
+        JsonObject object = response.readEntity(JsonObject.class);
+        assertThat(object.getInt("age"), is(4));
+        assertThat(object.getString("name"), is("Peter"));
     }
 
     @Test
-    public void test04_CreateAnimal(){
+    public void test03_CreateAnimal() {
         JsonObjectBuilder builder = Json.createObjectBuilder();
-        JsonObject farm = Json.createObjectBuilder().add("id", 1).add("location", "Linz").build();
-        JsonObject newAnimal = builder.add("name", "Musti").add("age", 6).add("fieldOfUse", "züchten").build();
-        Response response = this.target.path("animal/postpork").request().post(Entity.json(newAnimal));
+        JsonObject newAnimal = builder
+                .add("name", "Alex")
+                .add("age", 2)
+                .add("fieldOfUse", "züchten")
+                .build();
+        Response response = this.target.path("/animal/new/pork").request().post(Entity.json(newAnimal));
+        assertThat(response.getStatus(), is(201));
+        JsonObject animal = this.client.target(response.getLocation()).request(MediaType.APPLICATION_JSON).get().readEntity(JsonObject.class);
+        assertThat(animal.getInt("age"), is(2));
+        assertThat(animal.getString("name"), containsString("Alex"));
+
+        tmpAnimalId = animal.getInt("id");
+    }
+
+    @Test
+    public void test04_UpdateAnimal() {
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        JsonObject animal = builder
+                .add("name", "Manuel")
+                .add("age", 4)
+                .add("fieldOfUse", "schlachten")
+                .build();
+        Response response = this.target.path("/animal/update/pork/6").request().put(Entity.json(animal));
         assertThat(response.getStatus(), is(200));
 
-        long id = Long.valueOf(response.readEntity(String.class));
-        JsonObject animal = this.target.path("/animal/findById/" + id).request(MediaType.APPLICATION_JSON).get().readEntity(JsonObject.class);
-        assertThat(animal.getInt("age"), is(6));
+        JsonObject updatedAnimal = this.target.path("/animal/find/6").request(MediaType.APPLICATION_JSON).get().readEntity(JsonObject.class);
+        assertThat(updatedAnimal.getInt("age"), is(4));
+
+        animal = builder
+                .add("name", "Manuel")
+                .add("age", 3)
+                .add("fieldOfUse", "schlachten")
+                .build();
+        this.target.path("/animal/update/pork/6").request().put(Entity.json(animal));
     }
 
     @Test
-    public void test05_UpdateAnimal(){
-        JsonObject farm = Json.createObjectBuilder().add("id", 1).add("location", "Linz").build();
-        JsonObject animal = Json.createObjectBuilder().add("name", "Ted").add("age", 2).add("farm", farm).build();
-
-        this.target.path("/animal/put/" + 6).request().put(Entity.json(animal));
-        JsonObject response = this.target.path("/animal/findById/" + 6).request(MediaType.APPLICATION_JSON).get().readEntity(JsonObject.class);
-        assertThat(response.getInt("age"), is(2));
+    public void test05_DeleteAnimal() {
+        Response response = this.target.path("/animal/delete/" + tmpAnimalId).request().delete();
+        assertThat(response.getStatus(), is(200));
+        response = this.target.path("/animal/delete/" + tmpAnimalId).request().delete();
+        assertThat(response.getStatus(), is(200));
     }
 
     @Test
-    public void test06_DeleteAnimal(){
-        Response response = this.target.path("/animal/delete/" + 6).request().delete();
-        assertThat(response.getStatus(), is(Response.Status.NO_CONTENT.getStatusCode()));
+    public void test06_GetFarm() {
+        Response response = target.path("/farm/find/1").request(MediaType.APPLICATION_JSON).get();
+        assertThat(response.getStatus(), is(200));
+        JsonObject respObj = response.readEntity(JsonObject.class);
+        assertThat(respObj.getString("location"), is("Linz"));
+
+        JsonArray animals = respObj.getJsonArray("animals");
+        assertThat(animals.size(), greaterThan(1));
+    }
+
+    @Test
+    public void test07_GetField() {
+        Response response = target.path("/field/find/1").request().get();
+        assertThat(response.getStatus(), is(200));
+        JsonObject field = response.readEntity(JsonObject.class);
+        assertThat(field.getInt("hectare"), is(4));
+        assertThat(field.getString("plantedSeeds"), is("Weizen"));
+    }
+
+    @Test
+    public void test08_CreateField() {
+        JsonObject field = Json.createObjectBuilder()
+                .add("hectare", 3)
+                .add("plantedSeeds", "Weizen")
+                .build();
+        Response response = target.path("/field/new").request().post(Entity.json(field));
+        assertThat(response.getStatus(), is(201));
+        JsonObject newField = this.client.target(response.getLocation()).request(MediaType.APPLICATION_JSON).get().readEntity(JsonObject.class);
+        assertThat(newField.getInt("hectare"), is(3));
+        assertThat(newField.getString("plantedSeeds"), is("Weizen"));
+
+        tmpFieldId = newField.getInt("id");
+    }
+
+    @Test
+    public void test09_UpdateField() {
+        JsonObject team = Json.createObjectBuilder()
+                .add("hectare", 5)
+                .add("plantedSeeds", "Roggen")
+                .build();
+        Response response = target.path("/field/update/" + tmpFieldId).request().put(Entity.json(team));
+        assertThat(response.getStatus(), is(200));
+        response = target.path("/field/find/" + tmpFieldId).request().get();
+        assertThat(response.getStatus(), is(200));
+        JsonObject updatedField = response.readEntity(JsonObject.class);
+        assertThat(updatedField.getInt("hectare"), is(5));
+    }
+
+    @Test
+    public void test10_DeleteField() {
+        Response response = this.target.path("/field/delete/" + tmpFieldId).request().delete();
+        assertThat(response.getStatus(), is(200));
+        response = this.target.path("/field/delete/" + tmpFieldId).request().delete();
+        assertThat(response.getStatus(), is(200));
     }
 }
